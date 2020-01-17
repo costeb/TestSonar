ced3c34af13cb322c267ec1e7cae9fe3ca655937
Benjamin Coste <benjamincoste@outlook.fr>
2020-01-17 17:24:47 +0100

    Ajout d'un commentaire

--- src/Principale.java
+++ src/Principale.java
@@ -2,6 +2,7 @@
 public class Principale {
 	
 	public static void afficher() {
+		//On instancie un objet Seconde
 		Seconde seconde = new Seconde("Voici le chiffre huit:", 8);
 		System.out.println(seconde.getPhrase() + seconde.getEntier());
 	}
